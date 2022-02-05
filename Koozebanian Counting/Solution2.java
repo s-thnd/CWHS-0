acf63b190300396d453b24680f6c6b9e891b90d1
s-thnd <98992429+s-thnd@users.noreply.github.com>
2022-02-05 07:23:08 -0600

    Create Solution2.java

---
+++ Koozebanian Counting/Solution2.java
@@ -0,0 +1,12 @@
+//This solution iterates through 10 numbers and prints it out. This is a more concise and clean way to code this.
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++)
+            System.out.println(i);
+    }
+}
